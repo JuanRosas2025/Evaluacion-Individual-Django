3c991a7e40899a9c1f323d2108def54b4390b3c6
Juan Rosas <jrosas2025@alu.uct.cl>
2025-06-25 00:28:37 -0400

    Views configurado

--- paginas/views.py
+++ paginas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+def inicio(request):
+    return render(request, 'paginas/inicio.html')
+
+def acerca(request):
+    return render(request, 'paginas/acerca.html')
+
+def contacto(request):
+    return render(request, 'paginas/contacto.html')
 # Create your views here.
